7717fa39f951c4f8658270408300b3603a16b630
zmfl1230 <zmfl123097@gmail.com>
2020-12-10 20:33:22 +0900

    feat: add condition on shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,24 +6,22 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
   exit 1
 fi
 
-if [[ "$(jq -r ".head_commit.message")" =~ ^[a-z]{1,10}-pr:[[:space:]].+ ]]; then
-  echo "This push has not commits!"
-  exit 78
+if [[ "${{ commit_message }}" == ^[a-z]{1,10}-pr:[[:space:]].+ ]]; then
+  COMMIT_MESSAGE='$(jq -r "commit_message" "$GITHUB_EVENT_PATH" | head -1)'
+  REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+  DEFAULT_BRANCH=$(jq -r ".repository.default_branch" "$GITHUB_EVENT_PATH")
+
+  echo "~~~~ Data ~~~~"
+  echo "
+  title : $COMMIT_MESSAGE
+  ref   : $GITHUB_REF
+  "
+
+  RESPONSE_CODE=$(curl -o .output -s -w "%{http_code}\n" \
+    --data "{\"title\":\"$COMMIT_MESSAGE\", \"head\": \"$GITHUB_REF\", \"base\": \"$DEFAULT_BRANCH\"}" \
+    -X POST \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/repos/$REPO_FULLNAME/pulls")
+
 fi
-
-COMMIT_MESSAGE="$(jq -r ".head_commit.message" "$GITHUB_EVENT_PATH" | head -1)"
-REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
-DEFAULT_BRANCH=$(jq -r ".repository.default_branch" "$GITHUB_EVENT_PATH")
-
-echo "~~~~ Data ~~~~"
-echo "
-title : $COMMIT_MESSAGE
-ref   : $GITHUB_REF
-"
-
-RESPONSE_CODE=$(curl -o .output -s -w "%{http_code}\n" \
-  --data "{\"title\":\"$COMMIT_MESSAGE\", \"head\": \"$GITHUB_REF\", \"base\": \"$DEFAULT_BRANCH\"}" \
-  -X POST \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
-  "https://api.github.com/repos/$REPO_FULLNAME/pulls")
